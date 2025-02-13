3535f9b157f5a6d52656d9aa5c4a7ce22dcf9bf3
CasAmmerman <cassidyammerman02@gmail.com>
2025-02-12 21:49:45 -0500

    Breakables, Scoreboard and updates
    I created breakable objects and updated the environment to create a more platformer-like feel.

---
+++ Assets/BreakableObject.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+    public int pointValue = 10; // Set this in the Inspector for different objects
+}
---
+++ Assets/PlayerDestroy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerDestroy : MonoBehaviour
+{
+    public TMP_Text scoreText; // Assign this in the Unity Inspector
+    private int points = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Breakable"))
+        {
+            BreakableObject breakable = other.GetComponent<BreakableObject>();
+            if (breakable != null)
+            {
+                points += breakable.pointValue; // Add the object's points
+                Destroy(other.gameObject); // Destroy the object
+                UpdateScoreText(); // Update UI
+            }
+        }
+    }
+
+    void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + points;
+        }
+    }
+  
+}
