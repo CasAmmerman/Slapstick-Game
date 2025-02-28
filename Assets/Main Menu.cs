081ed5340b19d3fe642bd4d8089c6b7cd6029625
CasAmmerman <cassidyammerman02@gmail.com>
2025-02-28 12:06:09 -0500

    Main Menu

--- Assets/Main Menu.cs
+++ Assets/Main Menu.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void ExitGame()
     {
-        Application.Quit(); // Exits the game (only works in a built application)
-        Debug.Log("Game is exiting..."); // This helps when testing in the editor
+        Application.Quit(); 
+        Debug.Log("Game is exiting..."); 
     }
 }
